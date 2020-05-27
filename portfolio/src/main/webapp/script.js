d098416fce03a72392f2be753dd74af75085bc2a
Julia Liu <julia5sliu@gmail.com>
2020-05-26 19:47:50 -0700

    Add profile image, update website content, add styles

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -20,7 +20,8 @@ function addRandomGreeting() {
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  //const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const greeting = "Life is like a box of chocolates. You never know what you're gonna get."
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
